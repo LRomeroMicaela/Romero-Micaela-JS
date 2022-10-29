9e85f085a5c6879055e1ce2667cf3b1673e59724
Micaela Romero <rom_micaela@hotmail.com>
2022-10-28 22:52:35 -0300

    agregando close boton

--- carrito.js
+++ carrito.js
@@ -1,20 +1,56 @@
 
 //saco del localStorage el item seleccionado por el usuario
-let almacenados = [];
-let item = localStorage.getItem("lentes");
-   if (item){
-    almacenados = JSON.parse(localStorage.getItem("lentes"));
-};
-console.log(item);
-const {cardImg} = almacenados;
-console.log (cardImg);
+let almacenados = JSON.parse(localStorage.getItem("lentes"));
+//variable para borrar carrito, con close boton
 
-/*
-function mostrarImgDeProdSeleccionado(){
-    let divConImg = document.createElement("div")
-    divConImg.innerHTML += 
-    <img width="100%" src= alt="producto elegido"></img>
 
-document.getElementById("artElegido")
+extraerProdSeleccinado();
+
+//funcion para extraer los item del array almacenados
+function extraerProdSeleccinado() {
+	for (let item of almacenados) {
+		let { cardImg, cardPrecio, cardTitle } = item;
+		console.log(item);
+		console.log(cardImg, cardPrecio, cardTitle);
+		mostrarImgDeProdSeleccionado();
+	}
 }
-*/
+//armado seccion donde muestra producto elegido
+function mostrarImgDeProdSeleccionado(){
+    for (item of almacenados){
+    let divConImg = document.createElement("div")
+    divConImg.innerHTML += `
+        <div class="seleccionado">
+            <div class="seleccionado-img col-10"><img width="100%" src=${item.cardImg} alt="producto elegido"></div>
+            <div class="seleccionado-description col-10"><p>Descripción del producto seleccionado: ${item.cardTitle}</p></div>
+            <div class="seleccionado-material col-10"><p>Precio por unidad: ${item.cardPrecio}</p></div>
+        </div>
+    `
+    document.getElementById("artElegido").append(divConImg);
+    }
+   multiplicarPorLaCantDeseada();
+}
+
+//me falta restar a la cantidad seleccionada al stock 
+//y dar la opcion de la cantidad al usuario
+// validar input
+//crear un alert con los datos del comprador y lo elegido.
+
+function multiplicarPorLaCantDeseada(){
+  const canti = document.getElementById("cantidadAComprar")
+    canti.addEventListener(`input`, multiplicarPorElPrecio)
+    console.log (canti)
+  
+}
+
+
+//boton para borrar lo del carrito
+//funcion para vaciar carrito y localStorage
+const clearCarrito = document.getElementById(`boton-close`);
+    clearCarrito.addEventListener(`click`, clearHTML);
+function clearHTML(){
+    const eliminarloTodo = localStorage.removeItem(`lentes`);
+    artElegido.innerHTML = "";
+    almacenados = "";    
+}
+
--- index.js
+++ index.js
@@ -16,15 +16,19 @@ class Lentes{
 }
 
 //creacion de objetos de la clase Lentes
-const armazon1 = new Lentes(100,"./img/aviadorBless.jpg","Armazón de receta", "Bless", "Aviador", "Negro", 7500, 100);
+const armazon1 = new Lentes(100,"./img/aviadorBless.jpg","Armazón de receta", "Bless", "Aviador", "Negro", 7500, 10);
 const armazon2 = new Lentes(101, "./img/davorHSol.jpg", "Lentes de Sol", "Davor", "Envolvente", "Negro", 7500, 100);
-const armazon3 = new Lentes(102, "./img/hexagonalBless.jpg", "Armazón de receta", "Bless", "Hexagonal", "Dos colores", 6500, 100);
-const armazon4 = new Lentes(103, "./img/pinUp.jpg", "Armazón de receta", "Bless", "Pin Up", "Dorado y Negro", 9000, 100);
-const armazon5 = new Lentes(104, "./img/solMildura.jpg", "Lentes de Sol","Mildura", "Pin Up", "Dorado y Rosa", 10500, 100);
-const armazon6 = new Lentes(105, "./img/solMuzik.jpg", "Lentes de Sol", "Muzik", "Wayfarer", "Negro", 8500, 100);
+const armazon3 = new Lentes(102, "./img/hexagonalBless.jpg", "Armazón de receta", "Bless", "Hexagonal", "Dos colores", 6500, 10);
+const armazon4 = new Lentes(103, "./img/pinUp.jpg", "Armazón de receta", "Bless", "Pin Up", "Dorado y Negro", 9000, 10);
+const armazon5 = new Lentes(104, "./img/solMildura.jpg", "Lentes de Sol","Mildura", "Pin Up", "Dorado y Rosa", 10500, 10);
+const armazon6 = new Lentes(105, "./img/solMuzik.jpg", "Lentes de Sol", "Muzik", "Wayfarer", "Negro", 8500, 10);
 
 let lentesDisponibles = [armazon1, armazon2, armazon3, armazon4, armazon5, armazon6];
+
+//array que corresponde al articulo seleccionado por el cliente.
 const arrayProductos = [];
+
+//funcion para armar las card por JS
 function armadoCard(){
     for (let armazon of lentesDisponibles){
         let card = document.createElement("div")
@@ -34,23 +38,24 @@ function armadoCard(){
             <img src = ${armazon.img} class="card-img-top" alt = ${armazon.nombre}>
               <h5 class="card-title txt-login">${armazon.nombre}, ${armazon.marca}</h5>
               <p class="card-text txt-login">$${armazon.precio}</p>
-              <a id="btn-add" onclick ="armazon.restaStock()" href="#" class="btn btn-primary subm1">Agregar al carrito</a>  
-        </div>`; //ver onclick
+              <a id="btn-add" href="#" class="btn btn-primary subm1">Agregar al carrito</a>  
+        </div>`; 
         document.getElementById("container-productos").append(card); 
         card.className ="card col-10 col-md-3 img-cat main-img";   
     }
 }
 armadoCard();
 
-//variables
+//variable que selecciona todos los botones de las card
 const addButtons = document.querySelectorAll(`#btn-add`);
 
 //funcion creo el evento del click del boton y llamo a la funcion traer la card seleccionada por el usuario
 addButtons.forEach(button =>{
     button.addEventListener(`click`,addToCartClicked);
 });
-// funcion que trae la card y extraigo los items de la card que me sirven. Llamo a otra funcion para armar el carrito
 
+// funcion que trae la card y extraigo los items de la card que me sirven. Llamo a otra funcion para armar el carrito
+//guarda en el localStorage el prod seleccionado para pasarlo luego a la hoja carrito
 function addToCartClicked(event){
     const boton = event.target;
     const traerCardEntera = boton.closest(`.card`);
@@ -64,53 +69,3 @@ function addToCartClicked(event){
 
     localStorage.setItem('lentes', JSON.stringify(arrayProductos))
 };
-
-/*
-function addToCartClicked(event){
-    const boton = event.target;
-    const traerCardEntera = boton.closest(`.card`);
-
-    const cardTitle = traerCardEntera.querySelector(`.card-title`).textContent;
-    const cardPrecio = traerCardEntera.querySelector(`.card-text`).textContent;
-    const cardImg = traerCardEntera.querySelector(`.card-img-top`).src;
-
-    //addProdToCart(cardTitle, cardPrecio, cardImg); 
-};
-const setCarrito = objeto => {
-    //console.log(objeto)
-    const producto = {
-        id: objeto.querySelector('.btn-dark').dataset.id,
-        title: objeto.querySelector('h5').textContent,
-        precio: objeto.querySelector('p').textContent,
-        cantidad: 1
-    }
-
-    if (carrito.hasOwnProperty(producto.id)) {
-        producto.cantidad = carrito[producto.id].cantidad + 1
-    }
-
-    carrito[producto.id] = {
-        ...producto //aplicado spred
-    }
-
-    pintarCarrito()
-}
-
-
-//no darle importancia  a esto, estaba probando algo 
-const seleccionadoDelCatalogo = document.querySelector(`#seleccionado-del-catalogo`);
-
-function addProdToCart(titulo, precio, img){
-    let carritoShowAdd = document.createElement("tr")
-
-    carritoShowAdd.innerHTML += `
-        <th scope="row">1</th>
-        <td>${img}</td>
-        <td>${titulo}</td>
-        <td>${precio}</td>
-
-    `; 
-    document.getElementById("seleccionado-del-catalogo").append(carritoShowAdd);
-    
-}
-*/
